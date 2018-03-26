406ddb99ae7a31c06d28a432d45f2cf2eceb6fa2
Raoul1996 <baobaozhou199606@gmail.com>
2018-03-26 21:18:30 +0800

    backup.py version 3

--- backup.py
+++ backup.py
@@ -1,15 +1,25 @@
 # backup script
 import os
+import zipfile
 import time
 
 source = ['/Users/zhoubao/workplace/python/zip']
 
 target_dir = '/Users/zhoubao/workplace/python/backup'
+if not os.path.exists(target_dir):
+    os.mkdir(target_dir)
 today = target_dir + os.sep + time.strftime('%Y%m%d')
 now = time.strftime('%H%M%S')
-target = today + os.sep + now + '.zip'
+comment = input('Enter a comment --->')
+if len(comment) == 0:
+    target = today + os.sep + now + '.zip'
+else:
+    target = today + os.sep + now + '_' + \
+             comment.replace(' ', '_') + '.zip'
+
 if not os.path.exists(today):
     os.mkdir(today)
+    print('Successful created directory', today)
 
 zip_command = 'zip -r {0} {1}'.format(target, ' '.join(source))
 
